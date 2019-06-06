e1e529875328d1ea94354e5bafe957086093a29b
douglasbakkum <douglas.bakkum@shiftcrypto.ch>
2019-06-06 09:10:42 +0200

    main_new.js: whitespace

--- www/js/main_new.js
+++ www/js/main_new.js
@@ -319,7 +319,7 @@ function init()
     Display.registerTouch(ui.optionLegacyButton, function() {
         resetState();
         writeLocalData(function() {
-            window.location.href = "index.html"; 
+            window.location.href = "index.html";
         });
     });
     Display.registerTouch(ui.pairSuspiciousDisconnectButton, disconnect);
@@ -585,9 +585,9 @@ function disconnect() {
     hideOptionButtons();
     disableConnectOptionsButtons(true);
     resetState();
-    
+
     console.log('disconnected and deleted bitboxEncryptionKey');
-    // TODO: instead of erasing the legacy data immeditately, 
+    // TODO: instead of erasing the legacy data immeditately,
     // show an option to go back to the legacy pairing
     writeLocalData(startUp);
 }
@@ -760,15 +760,15 @@ function pairManual() {
 //
 
 function loadLocalData() {
-	try {
+    try {
         // debug in browser:
         // localData = {"channelID":"C26UoG8xA6f3YCXYxAQWFGim839XZSEYWA2qTm5rSoqJ","encryptionKey":"G5NOcIpbE5h8lyfhyP9i80ETX5ipGiOua4rM7V0Jzcc=","serverURL":"","bitboxEncryptionKey":"ac248c9305b72adcffe024f2b4d7e724bb9ab2ef8f84be926623e3450df6b43d","authenticationKey":"oJ+nFl22CHZq5DHWHAVKCC8Z5d0XdQSBKYfKbLDlK58=","bitpos":2,"bytepos":4,"hash_pubkey_bitbox":"dd4942a4cd396c72a4c4d63f2e41033564e16f5298a87d5e08daa48cc40e2db8"};
         window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function(dir) {
             dir.getFile("data.txt", {create:true}, function(file) {
-			    localDataFile = file;
-		        readLocalData();
+                localDataFile = file;
+                readLocalData();
             })
-	    })
+        })
     }
     catch(err) {
         console.log(err.message);
@@ -795,7 +795,7 @@ function readLocalData() {
 }
 
 function writeLocalData(callback) {
-	try {
+    try {
         // console.log("localdata:", localData);
         if (!localDataFile) return;
         localDataFile.createWriter(function(fileWriter) {
